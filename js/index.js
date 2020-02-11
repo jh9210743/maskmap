afad644bc526326dc03ebbd37c37ef5cbb71a283
Jamie <jh9210743@gmail.com>
2020-02-11 08:21:07 +0800

    html

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// const result = (async function() {
+//   try {
+//     const response = await axios.get(
+//       "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json"
+//     )
+//     console.log(response)
+//   } catch (error) {
+//     console.error(error)
+//     // throw error
+//   }
+// })()
